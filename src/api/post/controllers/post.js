e68c2a44f195a37493a133cb54efda9ebeb008ff
Mtmozart <mmsnborges@gmail.com>
2023-07-24 15:04:36 -0300

    put the controller

--- src/api/post/controllers/post.js
+++ src/api/post/controllers/post.js
@@ -1,9 +1,22 @@
-'use strict';
+"use strict";
 
 /**
  * post controller
+ *
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::post.post');
+module.exports = createCoreController("api::post.post", {
+  async create(ctx) {
+    ctx.query = { ...ctx.query, user: ctx.state.user.id };
+    var { id } = ctx.state.user; //ctx.state.user contains the current authenticated user
+    const response = await super.create(ctx);
+    const updatedResponse = await strapi.entityService.update(
+      "api::post.post",
+      response.data.id,
+      { data: { user: id } }
+    );
+    return updatedResponse;
+  },
+});
